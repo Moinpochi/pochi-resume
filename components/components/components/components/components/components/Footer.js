9dc5d038a3786fefc1b6ee32b51a2b723489fc8a
Moinpochi <pochimoin@gmail.com>
2025-08-15 11:41:07 +0530

    Create Footer.js

---
+++ components/components/components/components/components/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center py-4 mt-10">
+      <p>© {new Date().getFullYear()} Pochi Moin. All rights reserved.</p>
+    </footer>
+  );
+}
